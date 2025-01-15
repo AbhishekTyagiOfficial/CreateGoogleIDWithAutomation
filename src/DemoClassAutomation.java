94597e867b3336529eecf04f22605c3e5a4bbe20
Abhishek Tyagi <abhishektyagi99831@gmail.com>
2025-01-15 14:36:44 +0530

    inital commit

---
+++ src/DemoClassAutomation.java
@@ -0,0 +1,59 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class DemoClassAutomation {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\kajal\\Documents/chromedriver.exe");
+		
+//		create object
+		//ChromeDriver driver = new ChromeDriver();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://accounts.google.com/");
+		driver.manage().window().maximize();
+		Thread.sleep(3000);
+		driver.navigate().refresh();
+		
+		WebElement forget = driver.findElement(By.cssSelector("button[jsname='Cuz2Ue']"));
+		forget.click();
+		System.out.println("forget clicked.");
+		Thread.sleep(3000);
+
+		driver.navigate().back();
+		System.out.println("back Press clicked.");
+		Thread.sleep(3000);
+
+        WebElement createAccountButton = driver.findElement(By.xpath("//button[.//span[text()='Create account']]"));
+
+        // Click the button
+        createAccountButton.click();
+        System.out.println("create Account clicked.");
+
+		Thread.sleep(3000);
+		
+		
+		WebElement personalUseOption = driver.findElement(By.xpath("//li[@jsname='RZzeR']"));
+        personalUseOption.click();
+        System.out.println("Account clicked.");
+        
+        WebElement enterfname = driver.findElement(By.id("firstName"));
+        enterfname.sendKeys("sagar chutiya");
+		Thread.sleep(3000);
+		System.out.println("Name Clicked");
+		
+		WebElement enterlname = driver.findElement(By.id("lastName"));
+		enterlname.sendKeys("Paplu");
+		Thread.sleep(3000);
+		System.out.println("LName Clicked");
+
+		WebElement next = driver.findElement(By.cssSelector("span[jsname='V67aGc']"));
+		next.click();
+		System.out.println("next button Clicked");
+		//driver.close();
+	}
+
+}
