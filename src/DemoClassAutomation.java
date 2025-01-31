41dbcb81778af490ed96d93928484d1a8f337b00
Abhishek Tyagi <abhishektyagi99831@gmail.com>
2025-01-31 11:20:35 +0530

    Refactor DemoClassAutomation to improve element wait handling and replace deprecated methods; update input field selectors for better reliability; create GmailID Final Commit

--- src/DemoClassAutomation.java
+++ src/DemoClassAutomation.java
@@ -22,17 +22,17 @@ public class DemoClassAutomation {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://accounts.google.com/");
 		driver.manage().window().maximize();
-		//Thread.sleep(3000);
+		// Thread.sleep(3000);
 		driver.navigate().refresh();
 
 		WebElement forget = driver.findElement(By.cssSelector("button[jsname='Cuz2Ue']"));
 		forget.click();
 		System.out.println("forget clicked.");
-		//Thread.sleep(3000);
+		// Thread.sleep(3000);
 
 		driver.navigate().back();
 		System.out.println("back Press clicked.");
-		//Thread.sleep(3000);
+		// Thread.sleep(3000);
 
 		WebElement createAccountButton = driver.findElement(By.xpath("//button[.//span[text()='Create account']]"));
 
@@ -40,20 +40,21 @@ public class DemoClassAutomation {
 		createAccountButton.click();
 		System.out.println("create Account clicked.");
 
-		//Thread.sleep(3000);
+		// Thread.sleep(3000);
 
 		WebElement personalUseOption = driver.findElement(By.xpath("//li[@jsname='RZzeR']"));
 		personalUseOption.click();
 		System.out.println("Account clicked.");
 
-		WebElement enterfname = driver.findElement(By.id("firstName"));
+//		WebElement enterfname = driver.findElement(By.id("firstName"));
+		WebElement enterfname = driver.findElement(By.xpath("//input[@aria-label='First name']"));
 		enterfname.sendKeys("Test");
-		//Thread.sleep(3000);
+		// Thread.sleep(3000);
 		System.out.println("Name Clicked");
 
 		WebElement enterlname = driver.findElement(By.id("lastName"));
 		enterlname.sendKeys("Kumar");
-		//Thread.sleep(3000);
+		// Thread.sleep(3000);
 		System.out.println("LName Clicked");
 
 		WebElement next = driver.findElement(By.cssSelector("span[jsname='V67aGc']"));
@@ -68,48 +69,70 @@ public class DemoClassAutomation {
 		// Select an option from dropdown
 		Select select = new Select(monthDropdown);
 		select.selectByVisibleText("August"); // Select "March"
-	
+
 		WebDriverWait wait_day = new WebDriverWait(driver, Duration.ofSeconds(10));
 		WebElement days = wait_day.until(ExpectedConditions.elementToBeClickable(By.id("day")));
 		days.sendKeys("29");
 		System.out.println("Days Clicked");
-		
+
 		WebDriverWait wait_year = new WebDriverWait(driver, Duration.ofSeconds(10));
 		WebElement year = wait_year.until(ExpectedConditions.elementToBeClickable(By.id("year")));
 		year.sendKeys("1996");
 		System.out.println("Years Clicked");
-		
+
 		WebDriverWait wait_gen = new WebDriverWait(driver, Duration.ofSeconds(10));
 		WebElement gender = wait_gen.until(ExpectedConditions.visibilityOfElementLocated(By.id("gender")));
 		System.out.println("Gender Clicked");
-		
+
 		// Select use for option form dropdown
 		Select oselect = new Select(gender);
 		oselect.selectByIndex(2);
-		
+
 		WebElement next_b = driver.findElement(By.cssSelector(("span[jsname='V67aGc']")));
 		next_b.click();
-		System.out.println("next_B Clicked wEIpqb nQOrEb");
-		
-		
+		System.out.println("next_B Clicked");
+
 		WebDriverWait coustom_radio = new WebDriverWait(driver, Duration.ofSeconds(10));
 		WebElement cus_rad = coustom_radio.until(ExpectedConditions.elementToBeClickable(By.id("selectionc3")));
 		cus_rad.click();
-		
+
 		WebElement custom_id = driver.findElement(By.xpath("//input[@aria-label='Create a Gmail address']"));
-		custom_id.sendKeys("testingk1898");
+		custom_id.sendKeys("Enter_your_id");
 		System.out.println("Email Clicked");
-		
+
 		WebElement next_bt = driver.findElement(By.className("VfPpkd-vQzf8d"));
 		next_bt.click();
 		System.out.println("next_bt Clicked");
-		
-		WebElement custom_pass = driver.findElement(By.id("passwd"));
-		custom_pass.sendKeys("Testingk1898");
-		
-		WebElement custom_confirm_pass = driver.findElement(By.id("confirm-passwd"));
-		custom_confirm_pass.sendKeys("Testingk1898");
-		
+
+		WebDriverWait wait_pass = new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebElement custom_pass = wait_pass
+				.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@aria-label='Password']")));
+		custom_pass.sendKeys("Enter_your_pass");
+		System.out.println("Pass Clicked");
+
+		WebDriverWait wait_cpass = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+		WebElement custom_confirm_pass = wait_cpass
+				.until(ExpectedConditions.elementToBeClickable((By.xpath("//input[@aria-label='Confirm']"))));
+		custom_confirm_pass.sendKeys("Enter_your_confirm_pass");
+		System.out.println("Confirm_pass Clicked");
+
+		WebDriverWait wait_next = new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebElement nextBut = wait_next
+				.until(ExpectedConditions.elementToBeClickable((By.xpath("//span[text()='Next']"))));
+		nextBut.click();
+		System.out.println("NextBut Clicked");
+
+		WebDriverWait wait_no = new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebElement num = wait_no.until(ExpectedConditions.elementToBeClickable(By.id("phoneNumberId")));
+		num.sendKeys("xxxxxxxx");
+		System.out.println("Num Entered Clicked");
+
+		WebDriverWait wait_next1 = new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebElement nextBut1 = wait_next.until(ExpectedConditions.elementToBeClickable((By.xpath("//span[text()='Next']"))));
+		nextBut1.click();
+		System.out.println("NextBut Clicked");
+
 	}
 
 }
